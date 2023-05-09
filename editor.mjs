879769f2e55662f3e3b974d4528eefb3d3f817a1
Darius Foo <darius.foo.tw@gmail.com>
2023-05-09 11:21:40 +0800

    Atomic folded ranges

--- editor.mjs
+++ editor.mjs
@@ -2,14 +2,15 @@ import { EditorView, basicSetup } from "codemirror";
 // import { EditorView } from "codemirror";
 import { keymap, drawSelection } from "@codemirror/view";
 import { markdown } from "@codemirror/lang-markdown";
+// import { javascript } from "@codemirror/lang-javascript";
 import { vim, Vim } from "@replit/codemirror-vim";
 import { acceptCompletion } from "@codemirror/autocomplete";
-import { toggleFold } from "@codemirror/language";
+import { toggleFold, foldedRanges } from "@codemirror/language";
 // import { toggleFold } from "@codemirror/commands";
 
-/*
-
-# asd
+// let editor =
+new EditorView({
+  doc: `# asd
 asd
 ## asd
 aasd
@@ -20,20 +21,16 @@ asd
 ## asdkl
 kalsjld
 ### asldj
-kljhygyuguyd
-
-*/
-
-let editor = new EditorView({
+kljhygyuguyd`,
   extensions: [
     vim(),
     // keymap.of([{ key: "Ctrl-S", run: () => console.log("hi") }]),
     basicSetup,
     // drawSelection(),
     markdown(),
+    EditorView.atomicRanges.of((v) => foldedRanges(v.state)),
     // keymap.of([{ key: "Tab", run: acceptCompletion }]),
     keymap.of([{ key: "Tab", run: toggleFold }]),
-    keymap.of([{ key: "`", run: toggleFold }]),
     keymap.of([
       {
         key: "Alt-v", // only in insert mode
@@ -59,3 +56,6 @@ Vim.defineAction("forwardsearch", (view) => {
   console.log("normal mode");
 });
 Vim.mapCommand("za", "action", "forwardsearch");
+
+// Vim.defineAction("togglefold", toggleFold);
+// Vim.mapCommand("Tab", "action", "togglefold");
